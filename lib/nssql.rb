5d58f0506cf0e3e07e84451d57f7c31bb665d80c
Love Ottosson <love.ottosson@apoex.se>
2019-04-07 11:22:21 +0200

    Add NSSQL methods

--- lib/nssql.rb
+++ lib/nssql.rb
@@ -2,7 +2,54 @@ require "nssql/version"
 require "nssql/settings"
 require "nssql/table"
 
+require 'odbc_utf8'
+require 'tempfile'
+
 module NSSQL
   class Error < StandardError; end
-  # Your code goes here...
+
+  class << self
+    def select_array(query)
+      execute(query) do |result|
+        result.fetch_all
+      end
+    end
+
+    def select_to_file(query)
+      query = one_line_query(query)
+
+      isql_command = "isql -v Netsuite #{NSSQL::Settings.user} '#{NSSQL::Settings.password}' -b -q -d,"
+      iconv_command = 'iconv -f iso-8859-1 -t utf-8'
+
+      Tempfile.new.tap do |file|
+        system_call("echo \"#{query}\" | #{isql_command} | #{iconv_command} > #{file.path}")
+      end
+    end
+
+    def configure
+      NSSQL::Settings.configure
+    end
+
+    private
+
+    def execute(query)
+      netsuite_connection.prepare(query).execute.tap do |result|
+        yield result if block_given?
+
+        result.drop
+      end
+    end
+
+    def system_call(command)
+      `#{command}`
+    end
+
+    def one_line_query(query)
+      query.gsub("\n", ' ').gsub(/\s+/, ' ').strip
+    end
+
+    def netsuite_connection
+      ODBC.connect('NetSuite', NSSQL::Settings.user, NSSQL::Settings.password)
+    end
+  end
 end
--- test/nssql_test.rb
+++ test/nssql_test.rb
@@ -1,7 +1,77 @@
 require "test_helper"
 
 class NssqlTest < Minitest::Test
+  def setup
+    NSSQL::Settings.configure do |config|
+      config.user     = 'USER'
+      config.password = 'PASSWORD'
+    end
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::NSSQL::VERSION
+  end
+
+  def test_configure_delegation
+    NSSQL::Settings.stub(:configure, 'test-delegation') do
+      assert_equal 'test-delegation', NSSQL.configure
+    end
+  end
+
+  def test_select_to_file_produces_correct_command
+    expected_command = "echo \"SELECT 1+1\" | isql -v Netsuite USER 'PASSWORD' -b -q -d, | iconv -f iso-8859-1 -t utf-8"
+
+    system_mock = lambda do |command|
+      assert_equal expected_command, command.split(' > ').first
+
+      @file_path = command.split(' > ').last
+    end
+
+    NSSQL.stub :system_call, system_mock do
+      file = NSSQL.select_to_file('SELECT 1+1')
+
+      assert_equal file.path, @file_path
+    end
+  end
+
+  def test_select_to_file_one_line_query_squiggly
+    query = <<~SQL
+      SELECT
+        a, b
+      FROM
+        table
+    SQL
+
+    flattened_query = 'echo "SELECT a, b FROM table"'
+
+    system_mock = lambda do |command|
+      assert_equal flattened_query, command.split(' | ').first
+    end
+
+    NSSQL.stub :system_call, system_mock do
+      NSSQL.select_to_file(query)
+    end
+  end
+
+  def test_select_to_file_one_line_query_non_squiggly
+    query = <<-SQL
+      SELECT
+        a, b
+      FROM
+        table
+
+      ORDER BY
+        a
+    SQL
+
+    flattened_query = 'echo "SELECT a, b FROM table ORDER BY a"'
+
+    system_mock = lambda do |command|
+      assert_equal flattened_query, command.split(' | ').first
+    end
+
+    NSSQL.stub :system_call, system_mock do
+      NSSQL.select_to_file(query)
+    end
   end
 end
